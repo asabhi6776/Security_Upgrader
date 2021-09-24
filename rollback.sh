14e5ead813c991505104819284e081edb3fa0aee
Abhishek <johntheh4cker@gmail.com>
2021-09-24 13:30:14 +0530

    decoration in rollback

--- rollback.sh
+++ rollback.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
+clear
+cat rollascii
 echo 'Please select your distro'
-echo '[1] Ubuntu'
-echo '[2] CentOS'
-read -p "Please enter the no : " userinput
+echo "$(tput setaf 1) [1] Ubuntu"
+echo "$(tput setaf 2) [2] CentOS"
+read -p "$(tput sgr0) Please enter the no : " userinput
 
 if [ $userinput -eq 1 ]; then
      echo 'I will add the commands soon'
-elif [ condition ]; then
+elif [ $userinput -eq 2 ]; then
      yum history undo last
 else
     echo 'Please choose correct number.'
 fi
-
-
