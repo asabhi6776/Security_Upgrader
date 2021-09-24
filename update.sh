361ff26294c12311435878af492e6dfbba78edac
Abhishek <johntheh4cker@gmail.com>
2021-09-24 12:15:27 +0530

    decoration

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@ clear
 cat ascii
 echo "Select which distro you want to upgrade (Security Upgrades)"
 echo "$(tput setaf 1) [1] Ubuntu"
-echo "$(tput setaf 2) [2] CentOS";
+echo "$(tput setaf 2) [2] CentOS"
 read -p "$(tput sgr0) Select the no. which one you want to upgrade : " userinput
     if [ $userinput -eq 1 ]; then
          apt install unattanded-upgrades -y && unattanded-upgrades -d
