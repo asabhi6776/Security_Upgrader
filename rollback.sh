e3bb72cab9be044c0f4c44bf11692e0c2ca20efc
Abhishek <johntheh4cker@gmail.com>
2021-09-24 13:23:07 +0530

    rollback steps partial

---
+++ rollback.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo 'Please select your distro'
+echo '[1] Ubuntu'
+echo '[2] CentOS'
+read -p "Please enter the no : " userinput
+
+if [ $userinput -eq 1 ]; then
+     echo 'I will add the commands soon'
+elif [ condition ]; then
+     yum history undo last
+else
+    echo 'Please choose correct number.'
+fi
+
+
