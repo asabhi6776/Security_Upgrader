f008c8cef021d386d6fa095ed63e7a311d450d2c
Abhishek <johntheh4cker@gmail.com>
2021-09-24 11:42:34 +0530

    update script

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+clear
+cat ascii
+echo "Select which distro you want to upgrade (Security Upgrades)"
+echo "$(tput setaf 1) [1] Ubuntu"
+echo "$(tput setaf 2) [2] CentOS";
+read -p "$(tput sgr0) Select the no. which one you want to upgrade : " userinput
+    if [ $userinput -eq 1 ]; then
+         apt install unattanded-upgrades -y && unattanded-upgrades -d
+    elif [ $userinput -eq 2 ]; then
+         yum -y update --security
+    else
+        echo "Please choose correct number."
+    fi
+    
